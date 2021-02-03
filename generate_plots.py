3a75ffb9a9d95adfd6fc253b211f7a4d55dc9716
Albert Negura <Albert.Negura@Gmail.com>
2021-02-03 16:24:07 +0100

    More plotting

--- generate_plots.py
+++ generate_plots.py
@@ -1,9 +1,14 @@
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+
 import particle_swarm_optimization as pso
 import particle
 import numpy as np
 import math
 import matplotlib
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+from matplotlib import cm
+
 
 matplotlib.use("TkAgg")
 
@@ -13,14 +18,16 @@ bounds = []
 def plot_all(positions, cost, function):
     global bounds
     bounds = [-2.4, 2.4] if function == "rosenbrock" else [-5.12, 5.12]
-    fig, ((ax1, ax2),(ax3, ax4)) = plt.subplots(2, 2, sharex=False, sharey=False)
+    fig = plt.figure()
     fig.set_figheight(15)
     fig.set_figwidth(15)
+    ax1 = fig.add_subplot(221)
+    ax2 = fig.add_subplot(222, projection='3d')
+    ax3 = fig.add_subplot(223)
     contour_plot(positions, function, ax1)
-    surface_plot(positions, function, ax2, fig)
+    surface_plot(positions, function, ax2)
     cost_plot(positions, function, ax3)
     plt.show()
-
 
 def function_of(x, y, function):
     b = 1
@@ -42,20 +49,19 @@ def contour_plot(data, function, ax):
     zs = np.array(function_of(np.ravel(X), np.ravel(Y), function))
     Z = zs.reshape(X.shape)
 
-
-    ax.contour(X, Y, Z, levels=5, cmap='viridis')
+    ax.contour(X, Y, Z, levels=50, cmap='viridis')
     ax.title.set_text("2D Contour Plot of Objective Function")
 
-def surface_plot(data, function, ax, fig):
+def surface_plot(data, function, ax):
     x = np.arange(np.min(bounds), np.max(bounds), 0.05)
     y = np.arange(np.min(bounds), np.max(bounds), 0.05)
 
-    ax = fig.add_subplot(2, 2, 2, projection='3d')
     X, Y = np.meshgrid(x, y)
     zs = np.array(function_of(np.ravel(X), np.ravel(Y), function))
     Z = zs.reshape(X.shape)
 
-    ax.plot_surface(X, Y, Z, cmap='viridis', edgecolor='none', alpha=0.6)
+    ax.plot_surface(X, Y, Z, cmap=cm.nipy_spectral, alpha=0.6)
+    #ax.contour3D(X, Y, Z, 50, cmap='gray', linestyles="solid")
     ax.title.set_text("3D Plot of Objective Function")
 
 def cost_plot(data, function, ax):
@@ -70,4 +76,5 @@ def cost_plot(data, function, ax):
 
 
 if __name__ == "__main__":
-    plot_all([],[],"rosenbrock")
+    #plot_all([],[],"rastrigin")
+    plot_all([], [], "rosenbrock")
