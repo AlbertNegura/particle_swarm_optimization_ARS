c1a9d0dbdcea86ab254e846b168610ace4a90568
Sergi Nogués Farrés <nogues.s.6@gmail.com>
2021-02-04 12:00:23 +0100

    gradient descent rastrigin

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,7 +1,8 @@
 import numpy as np
 import random
+import math
 
-function = "rosenbrock" 
+function = "rastrigin" 
 bounds = [-2.4, 2.4] if function == "rosenbrock" else [-5.12, 5.12]
 position = [random.uniform(bounds[0],bounds[1]),random.uniform(bounds[0],bounds[1])]
 learning_rate = 0.01
@@ -30,19 +31,22 @@ def step(function, position):
         x = position[0]
         y = position[1]
 
-        if function == "rastrigin":  # rastrigin
+        if function == "rastrigin":
+            dx = 2*(x + 10*math.pi*np.sin(2*math.pi*x))
+            dy = 2*(y + 10*math.pi*np.sin(2*math.pi*y))
             
         if function == "rosenbrock":
             dx = 2*(2*(x**3) - 2*x*y + x)
             dy = 2*(y - x**2)
 
-            x_next = x - learning_rate*dx
-            y_next = y - learning_rate*dy 
-            return [x_next,y_next]
+        x_next = x - learning_rate*dx
+        y_next = y - learning_rate*dy 
+        return [x_next,y_next]
 
 data = [0 for x in range(iterations)]
+cost = evaluate(function,position)
 for i in range(iterations):
-    data[i] = position
+    data[i] = [position,cost]
     cost = evaluate(function,position)
     print("x {}, y {}, cost {}, iteration {}".format(position[0],position[1],cost,i))
 
