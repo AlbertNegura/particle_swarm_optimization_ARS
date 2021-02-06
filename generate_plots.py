25fa844bfb79265e5baebd1ef2dc4a38274011ca
Sergi Nogués Farrés <nogues.s.6@gmail.com>
2021-02-06 12:05:18 +0100

    small plot changes

--- generate_plots.py
+++ generate_plots.py
@@ -48,13 +48,13 @@ def contour_plot(data, function, ax):
 
     xs = data[:,:,0]
     ys = data[:,:,1]
-    scatters = ax.scatter(xs[0], ys[0], c="red", marker="o", vmin=0,vmax=data.shape[1])
+    scatters = ax.scatter(xs[0], ys[0], c="red", marker="o", vmin=0,vmax=data.shape[1],edgecolors="Black")
     lines = []
     for i in range(data.shape[1]):
         line = ax.plot(xs[0, i], ys[0, i], c="red", alpha=0.3)
         lines.append(line)
     ani = animation.FuncAnimation(fig, animate_contour, frames=50, fargs=[data,scatters, lines],
-                                            interval=50, blit=False, repeat=True)
+                                            interval=100, blit=False, repeat=True)
 
 def animate_contour(i, data,scatters, lines):
     plot_data = data[i,:,:2]
@@ -63,7 +63,7 @@ def animate_contour(i, data,scatters, lines):
         for lnum, line in enumerate(lines):
             xs = data[i - 5:i, lnum, :2]
             line[0].set_data(xs[:, 0], xs[:, 1])
-
+    
 def surface_plot(data, function, ax):
     global fig, scatters, ani2
     x = np.arange(np.min(bounds), np.max(bounds), 0.05)
@@ -100,13 +100,13 @@ def animate_surface(i, data, z_data, scatters, lines):
             line[0].set_data(xs[:, 0], xs[:, 1])
 
 def cost_plot(data, function, ax):
-    ax.set(xlim=[0,data.shape[0]], ylim=[np.argmin(data),np.argmax(data)],xlabel='Iterations',ylabel='Cost')
+    ax.set(xlim=[0,data.shape[0]],xlabel='Iterations',ylabel='Best Cost')
+    #ylim=[np.argmin(data),np.argmax(data)]
     ax.title.set_text('Min Cost Function')
     ax.plot(data, lw=3)
 
 #ani3 = animation.FuncAnimation(fig, animate3, frames=iterations, fargs=[],interval=100, blit=False, repeat=True)
 
 if __name__ == "__main__":
-    #plot_all([],[],"rastrigin")
     data,cost=pso.pso(function="rosenbrock", optimize_a=False)
     plot_all(np.asarray(data), cost, "rosenbrock")
