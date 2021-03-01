9ebddeabeae4d7e00c3b61884c00eb29040a5c80
I6145864 <a.negura@student.maastrichtuniversity.nl>
2021-03-01 16:53:11 +0100

    Working genetic algorithm

--- differential_evolution.py
+++ differential_evolution.py
@@ -90,7 +90,21 @@ def differential_evolution(crossover = 0.9, differential_weight = 0.8, populatio
     best_fitness = fitnesses[:,best_agent_id]
     return best_agent, best_fitness, agents_history #best_fitness_history
 
-def genetic_algorithm(crossover = 0.9, mutation = 0.1, population = 10, function="rosenbrock", max_iterations=5000, target_fitness=None, dimensions = 2):
+def cross_over(dad, mom):
+    if np.random.rand() > .5:
+        return [dad[0], mom[1]]
+    else:
+        return [mom[0], dad[1]]
+
+def mutate(parent, lower_bound, upper_bound):
+    if np.random.rand() > .5:
+        return [np.random.uniform(lower_bound, upper_bound), parent[1]]
+    else:
+        return [parent[0], np.random.uniform(lower_bound, upper_bound)]
+
+
+
+def genetic_algorithm(mutation = 0.1, population = 10, function="rosenbrock", max_iterations=5000, target_fitness=None, dimensions = 2, selection="elitism"):
     # check crossover in [0,1]
     # check weight in [0,2]
     # check pop >= 4
@@ -99,25 +113,20 @@ def genetic_algorithm(crossover = 0.9, mutation = 0.1, population = 10, function
 
     """
     :param crossover:
-    :param differential_weight:
+    :param mutation:
+    :param selection:
     :param pop_size:
     :return:
 
     - init agents x at random positions in search space
+    - calculate fitness value of each agent
     - while(not end) : (end can be number of iterations, adequate fitness reached, ...)
         - for each agent :
-            - pick 3 other agents at random
-            - pick a random index (bounded by size of problem)
-
-            - for each index :
-                - pick a uniformly distributed random number ri ~ U(0,1)
-                - if (ri < crossover) or (i = R ):
-                    - yi = ai + F x (bi - ci)
-                - else :
-                    - yi = xi
-
-            - if F(y) <= F(x) :
-                - replace agent x with the improved (or equal) agent y
+            - calculate whether selected (selection strategy)
+        - for each agent :
+            - if selected, use as parent
+            - otherwise, calculate cross-over with parents based on selection and mutation
+            - calculate fitness value in the new iteration
 
     - Pick agent with best fitness and return it as solution
 
@@ -131,32 +140,42 @@ def genetic_algorithm(crossover = 0.9, mutation = 0.1, population = 10, function
     iteration = 0
     best_fitness = None
     agents_history = np.zeros((max_iterations,len(agents),2),dtype=float)
+    fitness_history = np.zeros((max_iterations,len(agents)),dtype=float)
+    fitness_history[0] = [evaluate(function, agent) for agent in agents]
+    num_selected = int(population/5) if selection == "elitism" or selection == "roulette" else int(population/5*4) if selection == "steady" else int(population/2)
     while not end:
         iteration_history = []
-
+        selected_agents = []
+        # SELECTION
+        if selection == "elitism":
+            selected_agents = np.argpartition(fitness_history[iteration], num_selected)
+        elif selection == "tournament":
+            pass
+        elif selection == "roulette":
+            pass
+        elif selection == "steady":
+            selected_agents = np.argpartition(fitness_history[iteration], num_selected)
+        else: # in case of error, default to elitism
+            selected_agents = np.argpartition(fitness_history[iteration], num_selected)
         for x in range(len(agents)):
-            new_agent = [0 for _ in range(dimensions)]
-            temp = []
-            while len(temp) < 3:
-                rand_agent = random.randint(0, len(agents)-1)
-                if (rand_agent not in temp) or rand_agent != x:
-                    temp.append(rand_agent)
-            a = temp[0]
-            b = temp[1]
-            c = temp[2]
-            rand_index = 1 if dimensions < 2 else random.randint(1, dimensions)
+            if x in selected_agents[:num_selected]:
+                new_agent = agents[x]
+            else:
+                # CROSSOVER
+                dad = np.random.choice(selected_agents)
+                mom = np.random.choice(selected_agents)
+                while dad == mom:
+                    mom = np.random.choice(selected_agents)
+                new_agent = cross_over(agents[dad],agents[mom])
+                # MUTATION
+                if np.random.rand() < mutation:
+                    new_agent = mutate(new_agent, bounds[0], bounds[1])
 
-            for index in range(dimensions):
-                ri = np.random.uniform(0.0, 1.0)
-                if (ri < crossover) or (index == rand_index):
-                    new_agent[index] = agents[a][index] + differential_weight * (agents[b][index] - agents[c][index])
-                else:
-                    new_agent[index] = agents[x][index]
-            if evaluate(function, new_agent) <= evaluate(function, agents[x]):
-                agents[x] = new_agent
+            agents[x] = new_agent
             iteration_history.append(new_agent)
 
         agents_history[iteration] = np.clip(iteration_history,bounds[0],bounds[1])
+        fitness_history[iteration] = [evaluate(function, agent) for agent in agents]
         iteration += 1
         if (max_iterations != None) and (max_iterations == iteration):
             end = True
